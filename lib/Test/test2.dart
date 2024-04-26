ceaf4c6e98e1a3a558f0bee559bed8fbc4cdfa23
nguyen van nhat <nhatnvd00481@gmail.com>
2024-04-26 16:37:12 +0700

    aaaw

---
+++ lib/Test/test2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestScreen2 extends StatefulWidget {
+  const TestScreen2({super.key});
+
+  @override
+  State<TestScreen2> createState() => _TestScreen2State();
+}
+
+class _TestScreen2State extends State<TestScreen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
